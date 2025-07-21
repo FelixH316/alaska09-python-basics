b89185b078dc0eaba4324e90b574b26ac3a4f165
FelixH316 <3armenta@gmail.com>
2025-07-21 17:25:40 +0000

    Intro to python on using interactive notebooks from Jupyter

---
+++ section1/intro_01.py
@@ -0,0 +1,38 @@
+# *********************************************************
+# CLASS 01 SECTION 01
+# *********************************************************
+print("Hello World!")
+
+print("This is another block")
+
+print("Soy Felix y me encanta la ciencia de datos")
+
+
+# *********************************************************
+# CLASS 01 EXERCISES 01
+# *********************************************************
+# HW 01
+print("Escuela de Datos de Alura Latam")
+
+# HW 02
+print("Nombre: [Felix]")
+print("Apellido: [Armenta]")
+
+# HW 03
+name = "FELIX"
+for letter in name:
+    print(letter)
+
+# HW 04
+day = 21
+month = "Febrero"
+year = 1997
+print(f"{day} {month} {year}")
+
+# HW 05
+year = 2025
+print(f"Año actual: [{year}]")
+
+
+if __name__ == "__main__":
+    print("You're on intro_01.py file")
